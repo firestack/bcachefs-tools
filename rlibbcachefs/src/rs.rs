c04ff3532f8528cab7d2c5e00d037cfcee295b5b
Kayla Firestack <dev@kaylafire.me>
2021-10-05 09:11:59 -0400

    return two errors for read_super

--- rlibbcachefs/src/rs.rs
+++ rlibbcachefs/src/rs.rs
@@ -4,8 +4,12 @@ extern "C" {
 	pub static stdout: *mut libc::FILE;
 }
 
+pub enum ReadSuperErr { Io(std::io::Error), }
+
+type RResult<T> = std::io::Result<std::io::Result<T>>;
+
 #[tracing_attributes::instrument]
-pub fn read_super(path: &std::path::Path) -> std::io::Result<(bcachefs::bch_sb_handle, bcachefs::bch_opts)> {
+pub fn read_super(path: &std::path::Path) -> RResult<(bcachefs::bch_sb_handle, bcachefs::bch_opts)> {
 	// let devp = camino::Utf8Path::from_path(devp).unwrap();
 	
 	use std::os::unix::ffi::OsStrExt;
@@ -14,42 +18,37 @@ pub fn read_super(path: &std::path::Path) -> std::io::Result<(bcachefs::bch_sb_h
 	let mut opts = std::mem::MaybeUninit::zeroed();
 	let mut sb = std::mem::MaybeUninit::zeroed();
 	
-	use gag::{BufferRedirect};
-	// Stop libbcachefs from spamming the output
-	let gag = BufferRedirect::stderr().unwrap();
-	tracing::trace!("entering libbcachefs");
+	// use gag::{BufferRedirect};
+	// // Stop libbcachefs from spamming the output
+	// let gag = BufferRedirect::stderr().unwrap();
+	// tracing::trace!("entering libbcachefs");
+	
 	let ret = unsafe { crate::bcachefs::bch2_read_super(
 		path.as_ptr(),
 		opts.as_mut_ptr(),
 		sb.as_mut_ptr(),
 	)};
-	unsafe { libc::fflush(stdout); }
-	// Flush stdout so buffered output don't get printed after we remove the gag
-	let mut buf = String::new();
-
-	use std::io::Read;
-	let data = { gag.into_inner().read_to_string(&mut buf)? };
-	
-	tracing::trace!(result=%data, log=%buf);
+	let sb = unsafe { sb.assume_init() };
+	tracing::trace!(%ret);
 
 	match -ret {
-		0 => Ok( unsafe {(
-			sb.assume_init(),
-			opts.assume_init(),
-		)}),
 		libc::EACCES => Err(std::io::Error::new(
 			std::io::ErrorKind::PermissionDenied,
 			"Access Permission Denied",
 		)),
-		22 => Err(std::io::Error::new(
+		0 => Ok(Ok( unsafe {(
+			sb,
+			opts.assume_init(),
+		)})),
+		22 => Ok(Err(std::io::Error::new(
 			std::io::ErrorKind::InvalidData,
 			"Not a BCacheFS SuperBlock",
-		)),
+		))),
 		code => {
 			tracing::debug!(msg="BCacheFS return error code", ?code);
-			Err(std::io::Error::new(
+			Ok(Err(std::io::Error::new(
 				std::io::ErrorKind::Other,
 				"Failed to Read SuperBlock",
-		))}
+		)))}
 	}
 }
