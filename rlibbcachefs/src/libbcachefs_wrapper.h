bf7bf561436b0c9ecc80f736dab13025223fdf70
Kayla Firestack <dev@kaylafire.me>
2021-10-07 12:01:50 -0400

    add libcachefs and bio

--- rlibbcachefs/src/libbcachefs_wrapper.h
+++ rlibbcachefs/src/libbcachefs_wrapper.h
@@ -2,4 +2,7 @@
 #include "../libbcachefs/checksum.h"
 #include "../libbcachefs/bcachefs_format.h"
 #include "../libbcachefs/opts.h"
+#include "../libbcachefs.h"
 #include "../crypto.h"
+#include "../include/linux/bio.h"
+
