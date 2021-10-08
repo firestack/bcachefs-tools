ae8c9e7d436143b11b300a7064ca5eca9eb9587f
Kayla Firestack <dev@kaylafire.me>
2021-10-08 09:46:18 -0400

    run rustfmt

--- rlibbcachefs/src/rs.rs
+++ rlibbcachefs/src/rs.rs
@@ -4,29 +4,27 @@ extern "C" {
 	pub static stdout: *mut libc::FILE;
 }
 
-pub enum ReadSuperErr { Io(std::io::Error), }
+pub enum ReadSuperErr {
+	Io(std::io::Error),
+}
 
 type RResult<T> = std::io::Result<std::io::Result<T>>;
 
 #[tracing_attributes::instrument(skip(opts))]
 pub fn read_super_opts(path: &std::path::Path, mut opts: bcachefs::bch_opts) -> RResult<bcachefs::bch_sb_handle> {
 	// let devp = camino::Utf8Path::from_path(devp).unwrap();
-	
+
 	use std::os::unix::ffi::OsStrExt;
 	let path = std::ffi::CString::new(path.as_os_str().as_bytes())?;
-	
+
 	let mut sb = std::mem::MaybeUninit::zeroed();
-	
+
 	// use gag::{BufferRedirect};
 	// // Stop libbcachefs from spamming the output
 	// let gag = BufferRedirect::stderr().unwrap();
 	// tracing::trace!("entering libbcachefs");
-	
-	let ret = unsafe { crate::bcachefs::bch2_read_super(
-		path.as_ptr(),
-		&mut opts,
-		sb.as_mut_ptr(),
-	)};
+
+	let ret = unsafe { crate::bcachefs::bch2_read_super(path.as_ptr(), &mut opts, sb.as_mut_ptr()) };
 	tracing::trace!(%ret);
 
 	match -ret {
@@ -34,19 +32,18 @@ pub fn read_super_opts(path: &std::path::Path, mut opts: bcachefs::bch_opts) -> 
 			std::io::ErrorKind::PermissionDenied,
 			"Access Permission Denied",
 		)),
-		0 => Ok(Ok( unsafe {
-			sb.assume_init()
-		})),
+		0 => Ok(Ok(unsafe { sb.assume_init() })),
 		22 => Ok(Err(std::io::Error::new(
 			std::io::ErrorKind::InvalidData,
 			"Not a BCacheFS SuperBlock",
 		))),
 		code => {
-			tracing::debug!(msg="BCacheFS return error code", ?code);
+			tracing::debug!(msg = "BCacheFS return error code", ?code);
 			Ok(Err(std::io::Error::new(
 				std::io::ErrorKind::Other,
 				"Failed to Read SuperBlock",
-		)))}
+			)))
+		}
 	}
 }
 
