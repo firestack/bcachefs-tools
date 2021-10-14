4c67a0bfbdac3d0a0e198250c22325eafd68e03c
Kayla Firestack <dev@kaylafire.me>
2021-10-14 12:37:57 -0400

    add nixos Module and configuration

--- flake.nix
+++ flake.nix
@@ -15,6 +15,44 @@
 			version = "${builtins.substring 0 8 self.lastModifiedDate}-${self.shortRev or "dirty"}";
 
 			overlay = import ./nix/overlay.nix inputs;
+
+			nixosModule = self.nixosModules.bcachefs;
+			nixosModules.bcachefs = import ./rust-src/mount/module.nix;
+			nixosModules.bcachefs-enable-boot = ({config, pkgs, lib, ... }:{
+				# Disable Upstream NixOS Module when this is in use
+				disabledModules = [ "tasks/filesystems/bcachefs.nix" ];
+				# Import needed packages
+				nixpkgs.overlays = [ self.overlay ];
+
+				# Add bcachefs to boot and kernel
+				boot.initrd.supportedFilesystems = [ "bcachefs" ];
+				boot.supportedFilesystems = [ "bcachefs" ];
+			});
+
+			nixosConfigurations.netboot-bcachefs = self.systems.netboot-bcachefs "x86_64-linux";
+			systems.netboot-bcachefs = system: (nixpkgs.lib.nixosSystem { 
+					inherit system; modules = [
+						self.nixosModule 
+						self.nixosModules.bcachefs-enable-boot
+						("${nixpkgs}/nixos/modules/installer/netboot/netboot-minimal.nix")
+						({ lib, pkgs, config, ... }: {
+							# installation disk autologin
+							services.getty.autologinUser = lib.mkForce "root";
+							users.users.root.initialPassword = "toor";
+							
+							# Symlink everything together
+							system.build.netboot = pkgs.symlinkJoin {
+								name = "netboot";
+								paths = with config.system.build; [
+									netbootRamdisk
+									kernel
+									netbootIpxeScript
+								];
+								preferLocalBuild = true;
+							};
+						})
+					]; 
+				});
 		}
 		// utils.lib.eachSystem supportedSystems (system: 
 		let pkgs = import nixpkgs { 
