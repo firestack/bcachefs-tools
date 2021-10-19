1cf778f3b0bbf74eeb0e02de7781b086a5b1b23c
Kayla Firestack <dev@kaylafire.me>
2021-10-19 15:30:45 -0400

    flake.nix: add mount developer environment
    Signed-off-by: Kayla Firestack <dev@kaylafire.me>

--- flake.nix
+++ flake.nix
@@ -87,5 +87,6 @@
 
 			devShell = devShells.tools;
 			devShells.tools = pkgs.bcachefs.tools.override { inShell = true; };
+			devShells.mount = pkgs.bcachefs.mount.override { inShell = true; };
 		});
 }
