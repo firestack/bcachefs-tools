4c011f13c6b85b5b543569624f719c9cb03eab4a
Kayla Firestack <dev@kaylafire.me>
2021-11-11 08:01:11 -0500

    rust: update fields for show super

--- rust-src/rbcachefs/src/cmds/cmd_show_super.rs
+++ rust-src/rbcachefs/src/cmds/cmd_show_super.rs
@@ -1,12 +1,13 @@
-pub fn show_super() {
+pub fn show_super(opts: Options) {
 
 }
 
 use structopt::StructOpt;
 #[derive(StructOpt, Debug)]
 pub struct Options {
-   #[structopt(short, long)]
-   layouts: bool, 
-   #[structopt(short, long)]
-   fields: Vec<String>,
+	#[structopt(short, long)]
+	layouts: bool,
+
+	#[structopt(short, long)]
+	fields: Vec<String>,
 }
